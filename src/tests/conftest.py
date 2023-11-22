3d638476caae006e52b93ab9dabbf50aae0c2982
efi1 <efovadia@gmail.com>
2023-11-23 01:30:33 +0200

    update search_client in conftest.py with jobs whicj responsibel for table deletion, creation, data insertion, etc

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -1,29 +1,9 @@
 """Shared fixtures."""
 import json
-import os  # being used in func: pytest_addoption
-from pathlib import Path
-
+from importlib.resources import files
 from pytest import fixture
 from tests import settings
-from src.utils.utils import dict_to_obj, obj
-from src.clients.api_tests_client import TestsClient
-from src.clients.selen_tests_client import SelenTestsClient
-from jinja2 import Environment, FileSystemLoader
-
-# get all global settings for running the tests
-# settings_items = [i for i in settings.__dir__() if not i.startswith('_')]
-with open(eval(settings.cfg_global_file), "r+") as global_cfg:
-    GLOBAL = json.loads(global_cfg.read())
-
-
-def pytest_addoption(parser):
-    #   creating execution Pytest flags by a given settings file
-    for key, value in GLOBAL.items():
-        try:
-            value = eval(value)
-        except (SyntaxError, NameError, TypeError, ZeroDivisionError):
-            pass
-        parser.addoption(F"--{key}", action='store', default=value)
+from clients.api.search_web_activities import SearchWebsiteActivities
 
 
 @fixture(scope="function")
@@ -31,70 +11,51 @@ def test_name(request):
     return request.node.name
 
 
-@fixture(scope="session")
-def tests_raw_data(request: object) -> dict:
-    """
-    creating a dict data set with all the available flags and their default values
-    :param request: a pytest object
-    :return: a dict with all settings items
-    """
-    raw_data = dict()
-    for item in GLOBAL:
-        raw_data[item] = request.config.getoption(F"--{item}")
-    return raw_data
-
-
-@fixture(scope="session")
-def settings_data(tests_raw_data: dict) -> object:
-    """
-    Converting raw data of type dict to an object
-    :param tests_raw_data: raw data input
-    :return:
-    """
-    return dict_to_obj(tests_raw_data)
+def load_test_params(path):
+    with open(path) as file:
+        data = json.loads(file.read())
+    return data
 
 
 @fixture(scope="function")
-def cfg_data(tests_raw_data: dict, test_name: str) -> dict:
+def cfg_data(test_name: str) -> dict:
     """
     Rendering config data out of a template cfg file
     :param test_name:
     :param tests_raw_data: data to be rendered with in the cfg template file
-    :param cfg_name: cfg file name
-    :param cfg_dir: cfg folder path
-    :return: dict with the rendered data
+    :return: dict test's data
     """
-    cfg_name = F'{test_name}.cfg'
-    cfg_template_dir = eval(settings.cfg_tests_folder)
-    cfg_template_file = Path(cfg_template_dir).joinpath(cfg_name)
+    cfg_name = F'{test_name}.json'
+    cfg_template_dir = settings.cfg_tests_dir
+    cfg_template_file = files(cfg_template_dir).joinpath(cfg_name)
     if cfg_template_file.exists():
-        template_loader = FileSystemLoader(searchpath=cfg_template_dir)
-        template_env = Environment(loader=template_loader)
-        template = template_env.get_template(cfg_name)
-        cur = dict(tests_raw_data.items())
-        cfg_data_str = template.render(cur)
-        cfg_data_dict = json.load(cfg_data_str)
-        return dict_to_obj(cfg_data_dict)
+        return load_test_params(cfg_template_file)
 
 
 @fixture(scope="session")
-def selen_tests_client(settings_data: object) -> object:
+def search_client() -> object:
     """
-    instantiate the selen_tests_client
-    :param settings_data: global data arguments
+    instantiate the search_client
+    also Truncate, Delete, Create, Insert tables by request as a prerequisite to the test running
     :return: client instant
     """
-    selen_tests_client = SelenTestsClient(settings_data)
-    selen_tests_client.open_page
-    yield selen_tests_client
-    selen_tests_client.client_tear_down
-
-
-@fixture(scope="session")
-def api_tests_client(settings_data: object) -> object:
-    """
-    instantiate the api_tests_client
-    :param settings_data: global data arguments
-    :return: client instant
-    """
-    yield TestsClient(settings_data)
+    db_path = files(settings.db_client_dir).joinpath(settings.db_name)
+    data_jobs_path = files(settings.data_jobs_dir).joinpath(settings.data_jobs_fn)
+    search_client = SearchWebsiteActivities(db_path, data_jobs_path)
+    # if in settings, the tables are set to be removed
+    if settings.is_delete_and_recreate_tables == True:
+        search_client.delete_db_tables(settings.db_data_dir, settings.table_del_fn, force=False)
+        search_client.create_db_tables(settings.db_data_dir, settings.table_creation_fn, force=True)
+    # by default the table are truncated for every run - can be changed in the settings module (src.tests.settings)
+    elif settings.is_truncate_tables == True:
+        deleted_tables_list = settings.table_list.split(',')
+        search_client.truncate_tables(deleted_tables_list)
+    # if tables were removed or truncated it needed to repopulate
+    if any([settings.is_delete_and_recreate_tables, settings.is_truncate_tables]):
+        # insert refernce data to ranking_parameters as a precondition to running the tests.
+        search_client.insert_ranking_parameters(settings.db_data_dir, settings.rank_insert_fn, settings.rank_tn)
+        # run insert process job as a precondition to running the tests.
+        search_client.insert_products_job(settings.db_data_dir, settings.products_insert_fn, settings.products_tn)
+    yield search_client
+    # delete the file which uses to save websites_products.id list for updating search_engine_ranking table
+    search_client.tear_down
