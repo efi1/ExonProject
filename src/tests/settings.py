78ddd8fae4baeb80ef094480cb6cacee71839aeb
efi1 <efovadia@gmail.com>
2023-11-23 01:31:18 +0200

    update vars in settings

--- src/tests/settings.py
+++ src/tests/settings.py
@@ -1,4 +1,17 @@
-target_folder = "F'{Path(__file__).parent.parent}{os.sep}target'"
-target_file = "F'{Path(__file__).parent.parent}{os.sep}target{os.sep}data.json'"
-cfg_tests_folder = "F'{Path(__file__).parent.parent}{os.sep}cfg_tests'"
-cfg_global_file = "F'{Path(__file__).parent.parent}{os.sep}cfg_global{os.sep}Global.cfg'"
+cfg_tests_dir = 'data.cfg_tests'
+db_client_dir = 'clients.db'
+db_data_dir = 'data.db_data'
+db_name = 'searching_engine.db'
+table_del_fn = 'tables_deletion.json'
+table_creation_fn = 'tables_creation.json'
+products_tn = 'products'
+products_insert_fn = 'db_products_insertion.json'
+rank_insert_fn = 'db_data_insertion.json'
+rank_tn = 'ranking_parameters'
+data_jobs_fn = 'to_be_inserted_into_ranking.txt'
+data_jobs_dir = 'data.jobs_data'
+table_list = "websites, products, websites_products, search_engine_ranking, ranking_parameters"
+is_delete_and_recreate_tables = False
+is_truncate_tables = True
+
+
